6fb419d16ae1d508d6ccfe3862a4f1e15c1d33c0
murifq <muhamad.rifqi510@gmail.com>
2024-03-06 17:14:54 +0700

    [REFACTOR] Add PaymentStatus enum

---
+++ src/main/java/enums/PaymentStatus.java
@@ -0,0 +1,23 @@
+package enums;
+import lombok.Getter;
+
+@Getter
+public enum PaymentStatus {
+    SUCCESS("SUCCESS"),
+    REJECTED("REJECTED");
+
+    private final String value;
+
+    private PaymentStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains (String param) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.getValue().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
